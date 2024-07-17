db7e47c3bfb67aa8d9bef09b3ba16b20744fba82
JonathanC6 <131599780+JonathanC6@users.noreply.github.com>
2024-07-17 12:57:46 +0800

    Add files via upload

---
+++ FTP/FTPclient/ftpClient.cs
@@ -0,0 +1,19 @@
+﻿using FTPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPclient
+{
+    internal class ftpClient
+    {
+        static void Main(string[] args)
+        {
+            ftpClientLib ftpCli = new ftpClientLib();
+            ftpCli.connectToServer();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FTP/FTPserver/ftpServer.cs
@@ -0,0 +1,19 @@
+﻿using FTPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPserver
+{
+    internal class ftpServer
+    {
+        static void Main(string[] args)
+        {
+            ftpServerLib ftpSer = new ftpServerLib("127.0.0.1", 8888);
+            ftpSer.listenToClient();
+            Console.ReadKey();
+        }
+    }
+}
